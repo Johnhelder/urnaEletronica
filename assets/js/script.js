97edcf6c6960f31b10aa749e6ece1fc074941b36
John Helder <johnhelderjp@gmail.com>
2022-11-12 12:07:36 -0300

    Criando a função de dar inicio a votação

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,26 @@
+//Controle de interface
+
 let seuVotoPara = document.querySelector(".d-1-1 span");
 let cargo = document.querySelector(".d-1-2 span");
 let descricao = document.querySelector(".d-1-4");
 let aviso = document.querySelector(".d-2");
 let lateral = document.querySelector(".d-1-right");
 let numeros = document.queryCommandValue(".d-1-3");
+
+let etapaAtual = 0;
+
+function comecarEtapa(){
+  let etapa = etapas[etapaAtual];
+
+  let numeroHtml = '';
+
+  seuVotoPara.style.display = 'none';
+  cargo.innerHTML = etapa.titulo;
+  descricao.innerHTML = '';
+  aviso.style.display = 'none';
+  lateral.innerHTML ='';
+  numeros.innerHTML = numeroHtml;
+}
 
 //Função dos botões do teclado
 function clicou(n){
@@ -22,3 +39,5 @@ function corrige(n){
 function confirma(n){
   alert("Clicou em confirma");
 }
+
+comecarEtapa();
