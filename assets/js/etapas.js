c69668f4a3c64c121aad3fde941219181595885b
John Helder <johnhelderjp@gmail.com>
2022-11-11 21:05:54 -0300

    Criando array com os candidatos

---
+++ assets/js/etapas.js
@@ -0,0 +1,48 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos:[
+      {
+        numero: 38111,
+        nome: "Fulano de tal",
+        partido: "ABC",
+        fotos:[
+          {url: "38111.jpg", legenda: "Vereador"}
+        ]
+      },
+      {
+        numero: 77222,
+        nome: "Beltrano da silva",
+        partido: "DEFG",
+        fotos:[
+          {url: "77222.jpg", legenda: "Vereador"}
+
+        ]
+      },
+
+    ]
+  },
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos:[
+      {
+        nuemro: 99,
+        nome: "Ciclano",
+        partido: "ABC",
+        vice: "Cic",
+        fotos:[
+          {url: "99.jpg", legenda: "Prefeito"},
+          {url: "99_2.jpg", legenda: "Vice-Prefeito", small: true}
+        ]
+      },
+      {
+        numero: 84,
+        nome: "Zulano",
+
+      
+      }
+    ]
+  }
+]
