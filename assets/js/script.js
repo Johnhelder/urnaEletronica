343eb3c0b1d2875ce6a1a4b54a5107f4c83698ba
John Helder <johnhelderjp@gmail.com>
2022-11-12 11:48:08 -0300

    Criando a função de controle dos botões do teclado

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,21 @@ let descricao = document.querySelector(".d-1-4");
 let aviso = document.querySelector(".d-2");
 let lateral = document.querySelector(".d-1-right");
 let numeros = document.queryCommandValue(".d-1-3");
+
+//Função dos botões do teclado
+function clicou(n){
+  alert("Clicou em " +n);
+}
+
+//Funções dos botões de controle
+function branco(n){
+  alert("Clicou em Branco");
+}
+
+function corrige(n){
+  alert("Clicou em Corrige");
+}
+
+function confirma(n){
+  alert("Clicou em confirma");
+}
